fbebbcb89646204954664a386bdc80261a426667
Crystal Luo <niuniu8888@MacBook-Pro-2.fios-router.home>
2020-09-20 19:03:26 -0400

    original

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+i = int(input("Enter a number: "))
+for n in range(1,i+1):
+    if n%3 == 0:
+        print("Buzz")
+        continue
+    elif n%5 == 0:
+        print("Fizz")
+        continue
+    print(n)
+
+        
+
+           

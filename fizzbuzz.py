5734fbb65ce07b3f02e604d44a0a5a055882eb51
Crystal Luo <niuniu8888@MacBook-Pro-2.fios-router.home>
2020-09-22 14:04:17 -0400

    added fizzbuzz command

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,9 @@
 i = int(input("Enter a number: "))
 for n in range(1,i+1):
-    if n%3 == 0:
+    if n%3 == 0 and n%5 == 0:
+        print("FizzBuzz")
+        continue
+    elif n%3 == 0:
         print("Buzz")
         continue
     elif n%5 == 0:
